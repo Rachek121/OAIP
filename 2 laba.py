56b9ac3c9e43fe37b4af3537c951b8ac621a701b
dontv <dontvacban1@gmail.com>
2023-10-30 13:46:36 +0900

    11

---
+++ 2 laba.py
@@ -0,0 +1,5 @@
+b = 0
+while (x := float(input())) <= 36.6:
+    if x < 0:
+        b += 1
+        print(b)
---
+++ main.py
@@ -0,0 +1,5 @@
+while True:
+    a = input('Введи строку: ')
+    if a == '':
+        break
+    print('Столько ты написал: ', len(a))
