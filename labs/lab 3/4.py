05083e62bcda6f978569e3d47004b6bbe3f3da98
11 <dontvacban1@gmail.com>
2023-11-05 19:55:04 +0900

    11

--- labs/lab 3/4.py
+++ labs/lab 3/4.py
@@ -1,2 +1,8 @@
-n = list(map(int, input().split()))
-
+n = input('Введите числа: ')
+nl = n.split()
+nl = [int(num) for num in nl]
+s = nl[0]
+for i in nl:
+    if i < s:
+        s = i
+print('Наименьшее число:', s)
--- labs/lab 3/8.py
+++ labs/lab 3/8.py
@@ -1,7 +1,7 @@
-word = input("Введите слово:")
-shortest_word = word
-while word != "стоп":
-    if len(word) < len(shortest_word):
-        shortest_word = word
-        word = input("Введите слово:")
-        print("Самое короткое слово:", shortest_word)
+w = input("Введите слово:")
+sw = w
+while w != "стоп":
+    if len(w) < len(sw):
+        sw = w
+        w = input("Введите слово:")
+        print("Самое короткое слово:", sw)
