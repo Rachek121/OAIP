32a9818beea59c8b15a59514d68f0ed629cb3f40
11 <dontvacban1@gmail.com>
2023-11-05 19:59:01 +0900

    11

--- labs/lab 3/8.py
+++ labs/lab 3/8.py
@@ -1,7 +1,11 @@
-w = input("Введите слово:")
-sw = w
-while w != "стоп":
-    if len(w) < len(sw):
-        sw = w
-        w = input("Введите слово:")
-        print("Самое короткое слово:", sw)
+w = []
+while True:
+    w1 = input("Введите слово: ")
+    if w1 == 'стоп':
+        break
+    w.append(w1)
+sw = w[0]
+for w1 in w:
+    if len(w1) < len(sw):
+        sw = w1
+print('Самое короткое слово:', sw)
