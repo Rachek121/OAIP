3ce3c85dd476b1fa040aef67f6c8f7f958e7124d
11 <dontvacban1@gmail.com>
2023-12-07 22:53:15 +0900

    11

--- labs/(dop)lab 10/3.py
+++ labs/(dop)lab 10/3.py
@@ -2,11 +2,21 @@ import random
 words = ['яблоко']
 word = random.choice(words)
 hidden_word = [''] * len(word)
-while '' in hidden_word:
+attempts = 5
+print("Добро пожаловать в игру 'Угадай слово'!")
+while '' in hidden_word and attempts > 0:
     print(' '.join(hidden_word))
     guess = input("Угадайте букву: ")
-    for i in range(len(word)):
-        if word[i] == guess:
-            hidden_word[i] = guess
-print(' '.join(hidden_word))
-print("Поздравляю, вы угадали слово '{}'!".format(word))
+    if guess in word:
+        for i, letter in enumerate(word):
+            if letter == guess:
+                hidden_word[i] = guess
+        print("Поздравляю, вы угадали букву!")
+    else:
+        attempts -= 1
+        print("Увы, такой буквы в слове нет. У вас осталось {} попыток.".format(attempts))
+if '_' not in hidden_word:
+    print(' '.join(hidden_word))
+    print("Поздравляю, вы угадали слово '{}'!".format(word))
+else:
+    print("Увы, вы проиграли. Загаданное слово было '{}'.".format(word))
