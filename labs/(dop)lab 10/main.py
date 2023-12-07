cf65bcd74d8c14b8ff6818205af47f2b607dde18
11 <dontvacban1@gmail.com>
2023-12-07 22:57:15 +0900

    11

--- labs/(dop)lab 10/main.py
+++ labs/(dop)lab 10/main.py
@@ -1,2 +1,14 @@
 import random
-words = ""
+all_characters = 'abcdefghijklnopqrstuvwxyz'
+a2ll_characters = '1234567890'
+a3ll_characters = '!?%№*+=$#@{}[]()^/|.,'
+a4ll_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+long = input('длина пароля?' + "\n")
+long = int(long)
+password = ''
+for i in range(long):
+    password += random.choice(all_characters)
+    password += random.choice(a2ll_characters)
+    password += random.choice(a3ll_characters)
+    password += random.choice(a4ll_characters)
+    print(password)
