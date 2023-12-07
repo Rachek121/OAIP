26d855d225b8b84aacbfcb016b3e1f4665d4adba
11 <dontvacban1@gmail.com>
2023-12-07 18:38:40 +0900

    11

--- labs/lab 2/3.py
+++ labs/lab 2/3.py
@@ -1,9 +1,7 @@
 a = input()
 b = input()
 c = input()
-if a == 'раз' or a == '1':
-    if b == 'два' or b == '2':
-        if c == 'три' or c == '3':
-            print('ГОРИ')
+if a == 'раз' or a == '1' and b == 'два' or b == '2' and c == 'три' or c == '3':
+    print('ГОРИ')
 else:
     print('НЕ ГОРИ')
