a0288871c623db21176cf1f8049d421517ddc7bc
Rache <dontvacban1@gmail.com>
2024-03-02 12:25:37 +0900

    .

--- labs_4/lab 7/checking.py
+++ labs_4/lab 7/checking.py
@@ -1,7 +1,15 @@
-def checking(n, div=None):
-    if div is None:
-        div = n - 1
-    while div >= 2:
-        if n % div == 0:
-            print("Число не является простым")
-            return False
+def checking(n, i=2):
+    if n <= 2:
+        return n == 2
+    if n % i == 0:
+        return False
+    if i * i > n:
+        return True
+    return checking(n, i + 1)
+
+
+number = 17
+if checking(number):
+    print(f"{number} - простое число")
+else:
+    print(f"{number} - составное число")
